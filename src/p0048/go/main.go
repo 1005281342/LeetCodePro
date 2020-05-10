c467c7f95eb8ba8d9b94a8d0a80fd15102d222e6
v_jesonou <v_jesonou@tencent.com>
2020-05-10 09:59:15 +0800

    update

---
+++ src/p0048/go/main.go
@@ -0,0 +1,29 @@
+package main
+
+func rotate(matrix [][]int) {
+	//// 1. 2-5-8对称翻转
+	//n := len(matrix)
+	//for i := 0; i < n; i++ {
+	//	for j := 0; i < (n >> 1); j++ {
+	//		matrix[i][j], matrix[i][n-1-j] = matrix[i][n-1-j], matrix[i][j]
+	//	}
+	//}
+	//
+	//// 2. 以右对角线进行对称翻转(不好处理)
+
+	// 1. 4-5-6对称翻转
+	n := len(matrix)
+	for j := 0; j < n; j++ {
+		for i := 0; i < (n >> 1); i++ {
+			matrix[i][j], matrix[n-1-i][j] = matrix[n-1-i][j], matrix[i][j]
+		}
+	}
+
+	// 2. 以左对角线进行对称翻转
+	for i := 0; i < (n >> 1); i++ {
+		for j := 0; j < n; j++ {
+			matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
+		}
+	}
+
+}
